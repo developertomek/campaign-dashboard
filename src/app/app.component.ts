02b04dd05489f1edce39be6819b82fa92835833c
developertomek <tomek3646@gmail.com>
2023-07-23 15:11:10 +0200

    basic nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'campaign-dashboard';
+  events: string[] = [];
+  opened: boolean = true;
 }
