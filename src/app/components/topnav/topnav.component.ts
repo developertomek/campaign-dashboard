93d20eb8d7241ef94e2e19786ff9e1db7271870a
developertomek <wisniewskit94@gmail.com>
2023-08-01 14:10:34 +0200

    create topnav component

---
+++ src/app/components/topnav/topnav.component.ts
@@ -0,0 +1,19 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { UserNavType } from 'src/app/shared/types/user';
+
+@Component({
+  selector: 'app-topnav',
+  templateUrl: './topnav.component.html',
+  styleUrls: ['./topnav.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TopnavComponent {
+  @Input() userNavInfo: UserNavType | null = null;
+  @Output() toggleSidenav: EventEmitter<MouseEvent> = new EventEmitter();
+}
