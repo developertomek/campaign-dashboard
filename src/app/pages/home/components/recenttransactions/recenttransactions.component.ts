884d277ce5039544bafa307a9fbe01ddb02ef18d
developertomek <wisniewskit94@gmail.com>
2023-08-01 20:22:30 +0200

    add table with data and styles

--- src/app/pages/home/components/recenttransactions/recenttransactions.component.ts
+++ src/app/pages/home/components/recenttransactions/recenttransactions.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { ITransaction } from 'src/app/shared/types/transaction';
 
 @Component({
   selector: 'app-recenttransactions',
   templateUrl: './recenttransactions.component.html',
-  styleUrls: ['./recenttransactions.component.scss']
+  styleUrls: ['./recenttransactions.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RecenttransactionsComponent {
+export class RecenttransactionsComponent implements OnInit {
+  @Input() transactions: ITransaction[] | null = null;
 
+  columns: string[] = ['customer', 'status', 'price', 'product', 'date'];
+  dataSource: ITransaction[] = [];
+
+  drop(event: CdkDragDrop<string[]>): void {
+    moveItemInArray(this.columns, event.previousIndex, event.currentIndex);
+  }
+
+  ngOnInit(): void {
+    this.dataSource = this.transactions ?? [];
+  }
 }
