f9481e20ab98a185ab3bd16f76544544f4641409
developertomek <wisniewskit94@gmail.com>
2023-08-01 20:21:28 +0200

    add overview

---
+++ src/app/pages/home/components/overview/overview.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-overview',
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.scss'],
+})
+export class OverviewComponent {
+  @Input() data: any = {};
+  @Input() title: string = '';
+  @Input() icon: string = '';
+}
