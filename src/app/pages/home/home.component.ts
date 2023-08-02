9e6f2505d0b810eac6daca363b11101249bd16e9
developertomek <wisniewskit94@gmail.com>
2023-08-02 12:23:27 +0200

    add components to homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subject, takeUntil } from 'rxjs';
+import { TransactionsService } from 'src/app/shared/services/transactions.service';
+import { UserService } from 'src/app/shared/services/user.service';
+import { ITransaction } from 'src/app/shared/types/transaction';
+import { IUser } from 'src/app/shared/types/user';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  userData$: Observable<IUser> = this.userService.get();
+  userData: IUser | null = null;
+  recentTransactions$: Observable<ITransaction[]> =
+    this.transactionService.getRecent();
+  ngUnsubscribe: Subject<void> = new Subject();
 
+  constructor(
+    private userService: UserService,
+    private transactionService: TransactionsService
+  ) {}
+
+  ngOnInit(): void {
+    this.userData$.pipe(takeUntil(this.ngUnsubscribe)).subscribe((data) => {
+      this.userData = data;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
 }
