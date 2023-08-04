6cda2e84d1f4256bf31cb7a101dcab85fd86e40d
developertomek <wisniewskit94@gmail.com>
2023-08-04 07:17:50 +0200

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,25 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FormsModule } from '@angular/forms';
+import { TopnavComponent } from './components/topnav/topnav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CustomersComponent } from './pages/customers/customers.component';
+import { StatisticsComponent } from './pages/statistics/statistics.component';
+import { OverviewComponent } from './pages/home/components/overview/overview.component';
+import { RecenttransactionsComponent } from './pages/home/components/recenttransactions/recenttransactions.component';
+import { LeftnavComponent } from './components/leftnav/leftnav.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    TopnavComponent,
+    HomeComponent,
+    CustomersComponent,
+    StatisticsComponent,
+    OverviewComponent,
+    RecenttransactionsComponent,
+    LeftnavComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
