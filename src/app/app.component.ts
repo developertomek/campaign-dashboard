71b64f2a95dd48b0822fe1dbbc15c795bc3b6343
developertomek <wisniewskit94@gmail.com>
2023-08-04 07:16:53 +0200

    change styles, add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { UserNavType } from './shared/types/user';
+import { UserService } from './shared/services/user.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'campaign-dashboard';
-  events: string[] = [];
-  opened: boolean = true;
+export class AppComponent implements OnInit {
+  opened: boolean = false;
+  innerWidth: number = 0;
+  userNavInfo$: Observable<UserNavType> = this.userService.getNavInfo();
+
+  constructor(private userService: UserService) {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(): void {
+    this.checkWindowAndSetOpened();
+  }
+
+  ngOnInit(): void {
+    this.checkWindowAndSetOpened();
+  }
+
+  checkWindowAndSetOpened(): void {
+    if (window.innerWidth > 992) {
+      this.opened = true;
+    } else {
+      this.opened = false;
+    }
+  }
 }
